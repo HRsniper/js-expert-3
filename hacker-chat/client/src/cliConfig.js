e15456bb76b79d2696d393738d2559cfed47e519
Hércules Ramlow (HRsniper) <63937242+HRsniper@users.noreply.github.com>
2021-03-23 21:35:53 -0400

    paser arguments cli

---
+++ hacker-chat/client/src/cliConfig.js
@@ -0,0 +1,30 @@
+export default class CliConfig {
+  constructor({ username, room, hostUrl }) {
+    this.username = username;
+    this.room = room;
+
+    const { hostname, port, protocol } = new URL(hostUrl);
+    console.log({ hostname, port, protocol });
+    this.host = hostname;
+    this.port = port;
+    this.protocol = protocol.replace(/\W/, "");
+  }
+
+  static parseArguments(commands) {
+    const cmd = new Map();
+
+    for (const key in commands) {
+      const commandPrefix = "--";
+      const command = commands[key];
+      const index = parseInt(key);
+
+      if (!command.includes(commandPrefix)) continue;
+
+      const commandKey = command.replace(commandPrefix, "");
+      const commandValue = commands[index + 1];
+      cmd.set(commandKey, commandValue);
+    }
+
+    return new CliConfig(Object.fromEntries(cmd));
+  }
+}
--- hacker-chat/client/src/index.js
+++ hacker-chat/client/src/index.js
@@ -1,7 +1,11 @@
 import Events from "events";
+import CliConfig from "./cliConfig.js";
 import TerminalController from "./terminalController.js";
 
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
+console.log(config);
 const componentEmitter = new Events();
 const controller = new TerminalController();
 
-await controller.initializeTable(componentEmitter);
+// await controller.initializeTable(componentEmitter);
