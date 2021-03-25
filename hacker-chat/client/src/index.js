a29f5fa969b043f4a0752ccbac7dbb9cddfcd1bc
Hércules Ramlow (HRsniper) <63937242+HRsniper@users.noreply.github.com>
2021-03-25 13:16:39 -0400

    end of day 3

--- hacker-chat/client/src/index.js
+++ hacker-chat/client/src/index.js
@@ -2,6 +2,7 @@ import Events from "events";
 import CliConfig from "./cliConfig.js";
 import TerminalController from "./terminalController.js";
 import SocketClient from "./socket.js";
+import EventManager from "./eventManager.js";
 
 const [nodePath, filePath, ...commands] = process.argv;
 const config = CliConfig.parseArguments(commands);
@@ -10,6 +11,16 @@ const config = CliConfig.parseArguments(commands);
 const componentEmitter = new Events();
 const controller = new TerminalController();
 const socketClient = new SocketClient(config);
+const eventManager = new EventManager({ componentEmitter, socketClient });
+
+const events = eventManager.getEvents();
+socketClient.attachEvents(events);
+
+const data = {
+  roomId: config.room,
+  username: config.username
+};
+eventManager.joinRoomAndWaitForMessages(data);
 
 await socketClient.initialize();
 await controller.initializeTable(componentEmitter);
